406e424ddb373afd6fc65352420dd25bb4015f22
David Christofas <dchristofas@posteo.de>
2024-04-20 00:24:15 +0200

    refactor main function

--- cmd/strichliste/main.go
+++ cmd/strichliste/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"strings"
+	"sync"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -30,84 +38,148 @@ import (
 )
 
 func main() {
-	env, err := godotenv.Read(".env")
-	if err != nil {
-		log.Fatal(err)
+	if err := godotenv.Load(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
 	}
-	dbUrl, ok := env["DATABASE_URL"]
-	if !ok {
-		log.Fatal(".env is missing DATABASE_URL")
+
+	if err := run(context.Background(), os.Stdout, os.Args, os.Getenv); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context, w io.Writer, args []string, getenv func(string) string) error {
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
+
+	dbUrl := getenv("DATABASE_URL")
+	if strings.TrimSpace(dbUrl) == "" {
+		return errors.New(".env is missing DATABASE_URL")
 	}
 	db, err := dburl.Open(dbUrl)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	b, err := os.ReadFile("strichliste.yaml")
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	var yml map[string]any
 	err = yaml.Unmarshal(b, &yml)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	ss := settings.NewService(yml)
-	sh := settings.NewHandler(ss)
+	settingsHandler := settings.NewHandler(ss)
 
 	ar := arepo.New(db)
 	asvc := articles.NewService(ar)
-	ah := arest.NewHandler(asvc)
+	articlesHandler := arest.NewHandler(asvc)
 
 	ur := urepo.New(db)
 	usvc, err := users.NewService(ss, ur)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	uh := urest.NewHandler(usvc)
+	usersHandler := urest.NewHandler(usvc)
 
 	tr := trepo.New(db)
 	tsvc := transactions.NewService(tr, ur, ar, ss)
-	th := trest.NewHandler(tsvc)
+	transactionsHandler := trest.NewHandler(tsvc)
 
 	mr := mrepo.New(db)
 	msvc := metrics.NewService(mr)
-	mh := mrest.NewHandler(msvc)
+	metricsHandler := mrest.NewHandler(msvc)
 
+	srv := NewServer(
+		settingsHandler,
+		articlesHandler,
+		usersHandler,
+		transactionsHandler,
+		metricsHandler,
+	)
+	httpServer := &http.Server{
+		Addr:    net.JoinHostPort("127.0.0.1", "8081"),
+		Handler: srv,
+	}
+
+	go func() {
+		log.Printf("listening on %s\n", httpServer.Addr)
+		if err := httpServer.ListenAndServe(); err != nil && errors.Is(err, http.ErrServerClosed) {
+			fmt.Fprintf(os.Stderr, "error listening and serving: %s\n", err)
+		}
+	}()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		<-ctx.Done()
+		fmt.Println("shutdown received")
+		defer cancel()
+	}()
+	wg.Wait()
+	return nil
+}
+
+func NewServer(
+	sh settings.Handler,
+	ah arest.Handler,
+	uh urest.Handler,
+	th trest.Handler,
+	mh mrest.Handler,
+) http.Handler {
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-	r.Use(middleware.URLFormat)
-	r.Use(render.SetContentType(render.ContentTypeJSON))
 
-	r.Route("/api", func(r chi.Router) {
-		r.Route("/user", func(r chi.Router) {
-			r.Get("/", uh.GetAll)
-			r.Get("/{uid}", uh.FindById)
-			r.Post("/{uid}", uh.UpdateUser)
-			r.Route("/{uid}/transaction", func(r chi.Router) {
-				r.Get("/", th.GetUserTransactions)
-				r.Post("/", th.CreateTransaction)
-				r.Delete("/{tid}", th.DeleteTransaction)
+	addRoutes(
+		r,
+		sh,
+		ah,
+		uh,
+		th,
+		mh,
+	)
+
+	return r
+}
+
+func addRoutes(
+	router chi.Router,
+	sh settings.Handler,
+	ah arest.Handler,
+	uh urest.Handler,
+	th trest.Handler,
+	mh mrest.Handler,
+) {
+	router.Use(render.SetContentType(render.ContentTypeJSON))
+	router.Route("/api", func(router chi.Router) {
+		router.Route("/user", func(router chi.Router) {
+			router.Get("/", uh.GetAll)
+			router.Get("/{uid}", uh.FindById)
+			router.Post("/{uid}", uh.UpdateUser)
+			router.Route("/{uid}/transaction", func(router chi.Router) {
+				router.Get("/", th.GetUserTransactions)
+				router.Post("/", th.CreateTransaction)
+				router.Delete("/{tid}", th.DeleteTransaction)
 			})
-			r.Post("/", uh.CreateUser)
+			router.Post("/", uh.CreateUser)
 		})
-		r.Get("/settings", sh.GetSettings)
-		r.Route("/article", func(r chi.Router) {
-			r.Get("/", ah.List)
-			r.Post("/", ah.CreateArticle)
-			r.Post("/{aid}", ah.UpdateArticle)
-			r.Delete("/{aid}", ah.DeactivateArticle)
+		router.Get("/settings", sh.GetSettings)
+		router.Route("/article", func(router chi.Router) {
+			router.Get("/", ah.List)
+			router.Post("/", ah.CreateArticle)
+			router.Post("/{aid}", ah.UpdateArticle)
+			router.Delete("/{aid}", ah.DeactivateArticle)
 		})
-		r.Route("/metrics", func(r chi.Router) {
-			r.Get("/", mh.GetMetrics)
+		router.Route("/metrics", func(r chi.Router) {
+			router.Get("/", mh.GetMetrics)
 		})
 	})
-
-	if err := http.ListenAndServe(":8081", r); err != nil {
-		log.Fatal(err.Error())
-	}
 }
